c8696ae2ccde09b342ba59c7d9790add8228fc6f
qraxiss <qraxiss@gmail.com>
2024-04-21 15:19:50 +0300

    added redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,39 @@
+import React from 'react'
+import { Counter } from './features/counter/Counter'
+import './App.scss'
+
 import Layout from './layout'
 
-import { useSelector, useDispatch } from 'react-redux'
-import { products } from './features/products'
-
-import FancyButton from './components/button/fancy-button'
-
 function App() {
-    const dispatch = useDispatch()
-    const productsData = useSelector((state) => state.products.value)
-    console.log()
-
     return (
         <Layout>
-            <FancyButton
-                onClick={() => {
-                    dispatch(products())
-                }}
-                state="loading"
-            ></FancyButton>
-            <FancyButton
-                onClick={() => {
-                    console.log(productsData)
-                }}
-            ></FancyButton>
+            <div className="App">
+                <header className="App-header">
+                    {/* <img src={logo} className="App-logo" alt="logo" /> */}
+                    <Counter />
+                    <p>
+                        Edit <code>src/App.js</code> and save to reload.
+                    </p>
+                    <span>
+                        <span>Learn </span>
+                        <a className="App-link" href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">
+                            React
+                        </a>
+                        <span>, </span>
+                        <a className="App-link" href="https://redux.js.org/" target="_blank" rel="noopener noreferrer">
+                            Redux
+                        </a>
+                        <span>, </span>
+                        <a className="App-link" href="https://redux-toolkit.js.org/" target="_blank" rel="noopener noreferrer">
+                            Redux Toolkit
+                        </a>
+                        ,<span> and </span>
+                        <a className="App-link" href="https://react-redux.js.org/" target="_blank" rel="noopener noreferrer">
+                            React Redux
+                        </a>
+                    </span>
+                </header>
+            </div>
         </Layout>
     )
 }
