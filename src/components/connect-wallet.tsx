45bd213ceba33124685ac1801b5d957f377bf9d1
qraxiss <qraxiss@gmail.com>
2024-05-05 21:50:03 +0300

    feat: siwe running after success wallet connect.

--- src/components/connect-wallet.tsx
+++ src/components/connect-wallet.tsx
@@ -5,7 +5,7 @@ import { signInWithEthereumLocal } from "wallet/siwe";
 import { BrowserProvider } from "ethers";
 
 import { useAccount } from "wagmi";
-import { fetchNonceAsync, verifySignatureAsync } from "slices/wallet/thunk";
+import { verifySignatureAsync } from "slices/wallet/thunk";
 
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
@@ -16,48 +16,28 @@ export default function ConnectWallet() {
   const { open } = useWeb3Modal();
   const provider = new BrowserProvider(window.ethereum);
   const { address, chainId, status } = useAccount();
-  const { nonce, jwt } = useSelector((state: any) => state.wallet.data);
-
-  if (jwt) {
-    localStorage.setItem("jwt", jwt);
-  }
+  const { nonce } = useSelector((state: any) => state.wallet.data);
+  const { logged } = useSelector((state: any) => state.user.data);
 
   const dispatch: AppDispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(fetchNonceAsync());
-  }, [dispatch]);
+    if (status === "connected" && !logged) {
+      signInWithEthereumLocal(address, chainId!, nonce, provider).then(
+        (data) => {
+          dispatch(verifySignatureAsync(data));
+        }
+      );
+    }
+  }, [status]);
 
   return (
     <Button
       type="button"
       className="btn btn-icon btn-topbar btn-ghost-dark rounded-circle text-muted"
-      onClick={() => open()}
+      onClick={() => open({ view: "Connect" })}
     >
       <i className="bi bi-coin fs-20"></i>
     </Button>
-    // <>
-    //   <Button
-    //     variant="primary"
-    //     onClick={() => {
-    //       open();
-    //     }}
-    //   >
-    //     Open Connect Modal
-    //   </Button>
-    //   <Button
-    //     variant="secondary"
-    //     onClick={() => {
-    //       if (address)
-    //         signInWithEthereumLocal(address, chainId!, nonce, provider).then(
-    //           (data) => {
-    //             dispatch(verifySignatureAsync(data));
-    //           }
-    //         );
-    //     }}
-    //   >
-    //     Sign In With Ethereum
-    //   </Button>
-    // </>
   );
 }
